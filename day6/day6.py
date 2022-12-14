03a275fbeb1835a5482acd56d26260247b5f771c
Robert Lyon <robert.lyon93@gmail.com>
2022-12-14 22:12:50 +0000

    Completed day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -1,20 +1,31 @@
 
+def printCurrentMessageMarker(startOfMessageMarkerLength, startOfMessageMarker):
+    for k in range(0, startOfMessageMarkerLength):
+        print(datastream[startOfMessageMarker + k], end='')
+    
+
+def decodeMessage(datastream, startOfMessageMarkerLength):
+    for i in range(0, len(datastream) - 4):
+        listOfMessageMarker = []
+        for j in range(0, startOfMessageMarkerLength):
+            listOfMessageMarker.append(datastream[i + j])
+
+        duplicateCounter = 0
+        for j in range(0, len(listOfMessageMarker)):
+            if(listOfMessageMarker[j] in listOfMessageMarker[j+1:len(listOfMessageMarker)]):
+                duplicateCounter += 1
+                
+        if(duplicateCounter < 1):
+            printCurrentMessageMarker(startOfMessageMarkerLength, i)
+
+            print()
+            print(i + startOfMessageMarkerLength)
+            break
 
 
 with open("input.txt", 'r') as f:
 
     datastream = f.readline()
+    decodeMessage(datastream, 14)
 
-    for i in range(0, len(datastream) - 4):
-        listOfFour = []
-        for j in range(0, 4):
-            listOfFour.append(datastream[i + j])
 
-        duplicateCounter = 0
-        for j in range(0, len(listOfFour)):
-            if(listOfFour[j] in listOfFour[j+1:len(listOfFour)]):
-                duplicateCounter += 1
-        if(duplicateCounter < 1):
-            print(datastream[i] + datastream[i+1] + datastream[i+2]+ datastream[i+3])
-            print(i + 4)
-            break
